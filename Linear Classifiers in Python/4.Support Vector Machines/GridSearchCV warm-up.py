a1fa4c5226adf9b39225de7104d16c576caa4068
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:19:57 +0500

    Create GridSearchCV warm-up.py

---
+++ Linear Classifiers in Python/4.Support Vector Machines/GridSearchCV warm-up.py
@@ -0,0 +1,10 @@
+# Instantiate an RBF SVM
+svm = SVC()
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'gamma':[0.00001, 0.0001, 0.001, 0.01, 0.1]}
+searcher = GridSearchCV(svm, parameters)
+searcher.fit(X, y)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
