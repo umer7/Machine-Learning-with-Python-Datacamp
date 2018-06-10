dc6720483613ee7861ec781705081545ff09af17
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:23:51 +0500

    Create Sentiment analysis for movie reviews.py

---
+++ Linear Classifiers in Python/1.Applying logistic regression and SVM/Sentiment analysis for movie reviews.py
@@ -0,0 +1,15 @@
+# Instantiate logistic regression and train
+lr = LogisticRegression()
+lr.fit(X, y)
+
+# Predict sentiment for a glowing review
+review1 = "LOVED IT! This movie was amazing. Top 10 this year."
+review1_features = get_features(review1)
+print("Review:", review1)
+print("Probability of positive review:", lr.predict_proba(review1_features)[0,1])
+
+# Predict sentiment for a poor review
+review2 = "Total junk! I'll never watch a film by that director again, no matter how good the reviews."
+review2_features = get_features(review2)
+print("Review:", review2)
+print("Probability of positive review:", lr.predict_proba(review2_features)[0,1])
