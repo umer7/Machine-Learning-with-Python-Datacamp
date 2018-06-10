ad8656360ec4d6809e31ffd0cb6298d555ec0160
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:31:34 +0500

    Create Logistic regression and feature selection.py

---
+++ Linear Classifiers in Python/3.Logistic regression/Logistic regression and feature selection.py
@@ -0,0 +1,15 @@
+# Specify L1 regularization
+lr = LogisticRegression(penalty='l1')
+
+# Instantiate the GridSearchCV object and run the search
+searcher = GridSearchCV(lr, {'C':[0.001, 0.01, 0.1, 1, 10]})
+searcher.fit(X_train, y_train)
+
+# Report the best parameters
+print("Best CV params", searcher.best_params_)
+
+# Find the number of nonzero coefficients (selected features)
+best_lr = searcher.best_estimator_
+coefs = best_lr.coef_
+print("Total number of features:", coefs.size)
+print("Number of selected features:", np.count_nonzero(coefs))
