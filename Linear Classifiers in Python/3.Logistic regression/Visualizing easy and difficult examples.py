c448b92b4433554351756d5fae8bffdbd56a6f0d
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:33:23 +0500

    Create Visualizing easy and difficult examples.py

---
+++ Linear Classifiers in Python/3.Logistic regression/Visualizing easy and difficult examples.py
@@ -0,0 +1,14 @@
+lr = LogisticRegression()
+lr.fit(X,y)
+
+# Get predicted probabilities
+proba = lr.predict_proba(X)
+
+# Sort the example indices by their maximum probability
+proba_inds = np.argsort(np.max(proba,axis=1))
+
+# Show the most confident (least ambiguous) digit
+show_digit(proba_inds[-1], lr)
+
+# Show the least confident (most ambiguous) digit
+show_digit(proba_inds[0], lr)
