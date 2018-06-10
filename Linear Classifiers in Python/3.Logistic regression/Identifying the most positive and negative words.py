7fd96c4ffee820a6ce1c481212f6a8375b17fd15
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:32:06 +0500

    Create Identifying the most positive and negative words.py

---
+++ Linear Classifiers in Python/3.Logistic regression/Identifying the most positive and negative words.py
@@ -0,0 +1,15 @@
+# Get the indices of the sorted cofficients
+inds_ascending = np.argsort(lr.coef_.flatten()) 
+inds_descending = inds_ascending[::-1]
+
+# Print the most positive words
+print("Most positive words: ", end="")
+for i in range(5):
+    print(vocab[inds_descending[i]], end=", ")
+print("\n")
+
+# Print most negative words
+print("Most negative words: ", end="")
+for i in range(5):
+    print(vocab[inds_ascending[i]], end=", ")
+print("\n")
