b1071cc99b3e4e7d020544aec4d8a95f19333a39
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:19:12 +0500

    Create Effect of removing examples.py

---
+++ Linear Classifiers in Python/4.Support Vector Machines/Effect of removing examples.py
@@ -0,0 +1,15 @@
+# Train a linear SVM
+svm = SVC(kernel="linear")
+svm.fit(X,y)
+plot_classifier(X, y, svm, lims=(11,15,0,6))
+
+# Make a new data set keeping only the support vectors
+print("Number of original examples", len(X))
+print("Number of support vectors", len(svm.support_))
+X_small = X[svm.support_]
+y_small = y[svm.support_]
+
+# Train a new SVM using only the support vectors
+svm_small = SVC(kernel="linear")
+svm_small.fit(X_small, y_small)
+plot_classifier(X_small, y_small, svm_small, lims=(11,15,0,6))
