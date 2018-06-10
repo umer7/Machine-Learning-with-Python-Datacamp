ca64869997deb8638022bbca2d211f63b74d08bc
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:26:35 +0500

    Create Changing the model coefficients.py

---
+++ Linear Classifiers in Python/2.Loss functions/Changing the model coefficients.py
@@ -0,0 +1,10 @@
+# Set the coefficients
+model.coef_ = np.array([[-1,1]])
+model.intercept_ = np.array([-3])
+
+# Plot the data and decision boundary
+plot_classifier(X,y,model)
+
+# Print the number of errors
+num_err = np.sum(y != model.predict(X))
+print("Number of errors:", num_err)
