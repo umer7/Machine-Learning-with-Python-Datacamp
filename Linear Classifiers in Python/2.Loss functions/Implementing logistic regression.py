8f1b9d52aa2f7f7a0b21357438ac0b7032f1ddb3
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:29:08 +0500

    Create Implementing logistic regression.py

---
+++ Linear Classifiers in Python/2.Loss functions/Implementing logistic regression.py
@@ -0,0 +1,15 @@
+# The logistic loss, summed over training examples
+def my_loss(w):
+    s = 0
+    for i in range(0,len(y)):
+        raw_model_output = w@X[i]
+        s = s + log_loss(raw_model_output * y[i])
+    return s
+
+# Returns the w that makes my_loss(w) smallest
+w_fit = minimize(my_loss, X[0]).x
+print(w_fit)
+
+# Compare with scikit-learn's LogisticRegression
+lr = LogisticRegression(fit_intercept=False, C=1000000).fit(X,y)
+print(lr.coef_)
