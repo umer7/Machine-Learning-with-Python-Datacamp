2a4f9695e96f9b61a02c122b476064954a59e732
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:32:48 +0500

    Create Regularization and probabilities.py

---
+++ Linear Classifiers in Python/3.Logistic regression/Regularization and probabilities.py
@@ -0,0 +1,10 @@
+# Set the regularization strength
+model = LogisticRegression(C=0.1)
+
+# Fit and plot
+model.fit(X,y)
+plot_classifier(X,y,model,proba=True)
+
+# Predict probabilities on training points
+prob = model.predict_proba(X)
+print("Maximum predicted probability", np.max(prob))
