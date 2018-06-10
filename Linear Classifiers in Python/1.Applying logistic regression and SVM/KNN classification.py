632307541c572ad46edf25cd201526cd52bb926f
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:22:00 +0500

    Create KNN classification.py

---
+++ Linear Classifiers in Python/1.Applying logistic regression and SVM/KNN classification.py
@@ -0,0 +1,9 @@
+from sklearn.neighbors import KNeighborsClassifier
+
+# Create and fit the model
+knn = KNeighborsClassifier()
+knn.fit(X_train, y_train)
+
+# Predict on the test features, print the results
+pred = knn.predict(X_test)[0]
+print("Prediction for test example 0:", pred)
