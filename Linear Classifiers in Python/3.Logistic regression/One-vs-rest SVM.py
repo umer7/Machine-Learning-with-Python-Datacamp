ff8939ef75b882c249e611877b78591c318ed3d9
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:35:24 +0500

    Create One-vs-rest SVM.py

---
+++ Linear Classifiers in Python/3.Logistic regression/One-vs-rest SVM.py
@@ -0,0 +1,7 @@
+# We'll use SVC instead of LinearSVC from now on
+from sklearn.svm import SVC
+
+# Create/plot the binary classifier (class 1 vs. rest)
+svm_class_1 = SVC()
+svm_class_1.fit(X_train, y_train==1)
+plot_classifier(X_train, y_train==1, svm_class_1)
