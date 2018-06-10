795820c82d7a7a1bafd5f581d8a9d40b0b0f2b88
Umer Farooq <k142229@nu.edu.pk>
2018-06-10 15:33:55 +0500

    Create Fitting multi-class logistic regression.py

---
+++ Linear Classifiers in Python/3.Logistic regression/Fitting multi-class logistic regression.py
@@ -0,0 +1,13 @@
+# Fit one-vs-rest logistic regression classifier
+lr_ovr = LogisticRegression()
+lr_ovr.fit(X_train, y_train)
+
+print("OVR training accuracy:", lr_ovr.score(X_train, y_train))
+print("OVR test accuracy    :", lr_ovr.score(X_test, y_test))
+
+# Fit softmax classifier
+lr_mn = LogisticRegression(multi_class="multinomial", solver="lbfgs")
+lr_mn.fit(X_train, y_train)
+
+print("Softmax training accuracy:", lr_mn.score(X_train, y_train))
+print("Softmax test accuracy    :", lr_mn.score(X_test, y_test))
